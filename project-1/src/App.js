9c5ac5094e50344c51e1e7a4f0282b928b5330bc
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-01-30 22:42:43 -0300

    componentes statefull

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+
+//componente de classe stateless
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +23,14 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
+
+
+// function App() {
+//   return (
+//   );
+// }
 
 export default App;
