d62ce68ce63ea1077d8e25d527e24a2bb63a9ef7
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-02-01 00:02:25 -0300

    componentes de classe com estado (statefull)

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -5,21 +5,41 @@ import { Component } from 'react';
 
 //componente de classe stateless
 class App extends Component {
+  //para aproximar classes das funcoes
+    state = {
+      name: 'Joao Luiz',
+      counter: 0
+    };
+
+  handlePClick = () => {
+    this.setState( { name: 'Beatryz' })
+  }
+
+  //arrow function puxa do proprio elemento pai
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 })
+  }
+
   render() {
+    const { name, counter } = this.state;
+
     return(
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handlePClick}>
+          {name} {counter}
         </p>
         <a
+          onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Este é o link
         </a>
       </header>
     </div>
