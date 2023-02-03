f54e4f29882980813507b2b81d528bbbb4273b0d
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-02-02 21:21:55 -0300

    lifeycle methods

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { Component } from 'react';
 
-
 //componente de classe stateless
 class App extends Component {
   //para aproximar classes das funcoes
     state = {
+      counter: 0,
       posts: [
         {
           id: 1,
@@ -21,15 +21,40 @@ class App extends Component {
           id: 3,
           title: 'titulo 3',
           body: 'corpo 3'
-        }
+        },
       ]
     };
+    timeoutUpdate = null;
+
+    //metodo do tipo render
+    //com tempo para minhas informacoes aparecem, 5 segundos
+    componentDidMount() {
+      this.handleTimeout()
+    }
+
+    componentDidUpdate() {
+      this.handleTimeout()
+    }
+
+    componentWillUnmount() {
+      clearTimeout(this.timeoutUpdate)
+    }
+
+    handleTimeout = () => {
+      const { posts, counter } = this.state;
+      posts[0].title = 'o titulo mudou';
+
+     this.timeoutUpdate = setTimeout(() => {
+        this.setState({ posts, counter: counter + 1 })
+     }, 1000)
+    }
 
   render() {
-    const { posts } = this.state;
-
+    const { posts, counter } = this.state;
+    
     return(
       <div className="App">
+        <h1>{ counter }</h1>
         {/* retornando os objetos da array */}
         {posts.map(post => (
         <>
