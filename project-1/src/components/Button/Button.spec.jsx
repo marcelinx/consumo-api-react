4d8f3c948408fa87dca83b35b7173136ea360f4b
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-02-21 19:48:13 -0300

    adicionado testes com jest, junto com props

--- project-1/src/components/Button/Button.spec.jsx
+++ project-1/src/components/Button/Button.spec.jsx
@@ -29,9 +29,13 @@ describe('<Button />', () => {
     });
     it('should be disabled false', () => {
         render(<Button text="Load more" disabled={false}/>);
-
         const button = screen.getByRole('button', { name: /load more/i }); 
-
         expect(button).toBeEnabled();
     });
+
+    it('should match snapshot', () => {
+        const fn = jest.fn();
+        const { container } = render(<Button text="Load more" disabled={false} onClick={fn} />);
+        expect(container.firstChild).toMatchSnapshot();
+    })
 });
