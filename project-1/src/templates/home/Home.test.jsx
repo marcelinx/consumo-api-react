6425a0bc3c72a87ca090fa960f148604a8e9d991
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-02-21 18:28:48 -0300

    trabalhando com tests em jest

---
+++ project-1/src/templates/home/Home.test.jsx
@@ -0,0 +1,6 @@
+//geralmente é escrito em ingles o nome dos testes.
+describe('<Home />', () => {
+  it('test one', () => {
+    expect(1).toBe(1)
+  });
+})
--- project-1/src/templates/home/index.jsx
+++ project-1/src/templates/home/index.jsx
@@ -1,32 +1,94 @@
-import { Component } from 'react';
+import { useEffect, useState, useCallback} from 'react';
+
 import './styles.css';
 
-export class Home extends Component {
-  state = {
-    counter: 0
-  }
+import { Posts } from '../../components/Posts';
+import { loadPosts } from '../../utils/load-posts'
+import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
 
-  handleClick = () => {
-    const { counter } = this.state;
+export const Home = () => {
 
-    this.setState(
-      (prevState, prevProps) => {
-        console.log('PREV', prevState.counter)
-        return { counter: prevState.counter + 1}
-    },
-      () => {
-      console.log('POST', this.state.counter)
-    })
-  }
+  //usando estados para trazer meus 'arrays'
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postPerPage] = useState(3);
+  const [searchValue, setSearchValue] = useState('');
+  
+  const noMorePost = page + postPerPage >= allPosts.length;
 
-  render () {
-    return(
-      <div className="container">
-        <h1>{this.state.counter}</h1>
-        <button onClick={this.handleClick}>incrementar</button>
+  const filteredPosts = !!searchValue ? 
+  allPosts.filter(post => {
+    return post.title.toLowerCase().includes(
+      searchValue.toLowerCase()
+      );
+  })
+  : posts;
+
+      const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+        const postsAndPhotos = await loadPosts()
+
+      setPosts(postsAndPhotos.slice(page, postPerPage));
+      setAllPosts(postsAndPhotos);
+    }, [])
+
+    useEffect(() => {
+      handleLoadPosts(0, postPerPage);
+    }, [handleLoadPosts, postPerPage])
+
+    // funcao para carregar posts
+    const loadMorePosts = () => {
+      const nextPage = page + postPerPage;
+      const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage)
+      posts.push(...nextPosts)
+
+      setPosts(posts);
+      setPage(nextPage);
+    } 
+
+    //funcao de mundanca enquanto pesquisa
+    const handleChange = (e) => {
+      const { value } = e.target;
+      setSearchValue(value)
+    }
+
+
+  return(
+    <section className='container'>
+      {/* criando pesquisas para buscar palavras em site */}
+    <div className="search-container">
+    {!!searchValue && (
+      <h1>Valor de pesquisa: {searchValue} </h1>
+    )}
+      <TextInput searchValue ={searchValue} handleChange={handleChange}/>
       </div>
-    )
-  }
+
+
+      {/* usando todos os posts filtrados em POSTS */}
+      {filteredPosts.length > 0 && (
+      <Posts posts={filteredPosts}/>
+      )}
+
+      {filteredPosts.length === 0 && (
+      <p>Nao existem posts</p>
+      )}
+
+
+      <div className="button-container">
+
+      {!searchValue && (
+      <Button 
+      text="Load more post"
+      onClick={loadMorePosts}
+      disabled={noMorePost}
+        />
+      )}
+      </div>
+    </section>
+  )
+
 }
 
 export default Home;
+
